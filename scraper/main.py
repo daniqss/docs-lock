4ad1c55332b289465958292a0993b3569a1c679f
daniqss <danielqueijo14@gmail.com>
2025-02-22 14:27:55 +0100

    feat(scraper): get package.json from repos

--- scraper/main.py
+++ scraper/main.py
@@ -63,6 +63,21 @@ def scrape_repositories(username: str) -> List[Dict[str, str]] | None:
 
     return [{"repository": f"https://github.com/{username}/{repo}", "lang": lang} for repo, lang in user_repositories]
 
+def search_for_packagejson(repo_url: str):
+    package_json_url = f"{repo_url.replace('github.com', 'raw.githubusercontent.com')}/main/package.json"
+
+    try:
+        response = requests.get(package_json_url)
+        response.raise_for_status()
+        try:
+            return response.json()
+        except json.JSONDecodeError:
+            return None
+    except requests.exceptions.RequestException:
+        return None
+
+    
+
 def main():
     if len(sys.argv) != 2:
         print("usage: python3 -m main <USERNAME>")
@@ -72,10 +87,15 @@ def main():
     repositories = scrape_repositories(username)
 
     if repositories is None:
-        print('{ "error": "Failed to fetch page or user does not exist" }') #Improved error message.
+        print('{ "error": "Failed to fetch page or user does not exist" }')
         sys.exit(1)
 
-    print(json.dumps(repositories, indent=2))
+    for repo in repositories:
+        if repo["lang"] == "TypeScript" or repo["lang"] == "JavaScript":
+            package_data = search_for_packagejson(repo["repository"])
+            if package_data:
+                print(json.dumps(package_data, indent=2))
+            
 
 if __name__ == "__main__":
     main()
