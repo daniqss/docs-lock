195e36e89ed6c2ab1c7f23437f6b3deed8e9d9e9
paulacarril <145045483+paulacarril@users.noreply.github.com>
2025-02-22 13:43:30 +0100

    refactor(client): adjust font size in SkillBox

--- client/src/components/SkillBox.tsx
+++ client/src/components/SkillBox.tsx
@@ -10,7 +10,8 @@ export default function SkillBox({ name }: SkillBoxProps) {
       to={`/skills/${name}`}
       className="w-full aspect-square flex items-center justify-center bg-gray-200 border border-gray-400 rounded-xl shadow-xl overflow-hidden"
     >
-      <h1 className="text flex font-bold text-gray-700 text-center px-2">{name}</h1>
+     <h1 className="flex font-bold text-gray-700 text-center px-2 text-[clamp(1rem,5vw,3rem)]">
+     {name}</h1>
     </Link>
   );
 }
