07b8c8f53a03e70b3701ba65ad20274f6ad67297
daniqss <danielqueijo14@gmail.com>
2025-02-22 23:23:31 +0100

    fix(#15): fix Header
    closes #15

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,6 @@ import { SessionContext, SessionContextType } from "../context/Session";
 import { Link } from "react-router-dom";
 // Add the font import in your CSS file or HTML file
 
-
 function Header() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement | null>(null);
@@ -30,39 +29,37 @@ function Header() {
       {/* Logo y título */}
       <section className="flex flex-row space-x-2 items-center">
         <Link to="/">
-        <h1 className="text-2xl lg:text-4xl font-bold text-gray-900 custom-font">
-  docs.lock
-</h1>
-
+          <h1 className="text-2xl lg:text-4xl font-bold text-gray-900 custom-font">
+            docs.lock
+          </h1>
         </Link>
       </section>
-  
+
       {/* Sección del perfil */}
       <section className="flex flex-row space-x-4 relative">
-        <button 
-  onClick={() => setDropdownOpen(!isDropdownOpen)} 
-  className="relative focus:outline-none active:transform-none"
->
-  <ProfileIcon className="w-8 h-8 cursor-pointer" />
-</button>
+        <button
+          onClick={() => setDropdownOpen(!isDropdownOpen)}
+          className="relative focus:outline-none active:transform-none"
+        >
+          <ProfileIcon className="w-8 h-8 cursor-pointer" />
+        </button>
 
-{isDropdownOpen && (
-  <div
-    ref={dropdownRef}
-    className="absolute right-0 top-12 shadow-lg rounded-md p-3 w-56 border bg-white"
-  >
-    <p className="font-semibold text-gray-900 text-left">
-      {"name: "} {user.name} {user.surname}
-    </p>
-    <p className="text-sm text-gray-700 text-left">
-      {"github: "} {user.github}
-    </p>
-  </div>
-)}
-
+        {isDropdownOpen && (
+          <div
+            ref={dropdownRef}
+            className="absolute right-0 top-12 shadow-lg rounded-md p-3 w-56 border bg-white"
+          >
+            <p className="font-semibold text-gray-900 text-left">
+              {"name: "} {user.Realname}
+            </p>
+            <p className="text-sm text-gray-700 text-left">
+              {"github: "} {user.gitUsername}
+            </p>
+          </div>
+        )}
       </section>
     </header>
-  );  
+  );
 }
 
 export default Header;
